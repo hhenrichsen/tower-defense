f9cdde5ca330348056f69d79b45186a17f2759d8
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2021-04-15 23:33:56 -0600

    Add level component.

---
+++ src/core/components/data/Level.ts
@@ -0,0 +1,21 @@
+import { lowerFirst } from "lodash";
+import { Component } from "../../ecs/Component";
+import { Entity } from "../../ecs/Entity";
+
+export interface LevelData extends Record<string, unknown> {
+  level: number; // level units
+}
+
+export type LevelEntity = Entity & { data: { level: LevelData } };
+
+export class Level extends Component {
+  public getName(): string {
+    return lowerFirst(this.constructor.name);
+  }
+
+  protected defaultData(): LevelData {
+    return { level: 1 };
+  }
+}
+
+export const LevelComponent = new Level();
