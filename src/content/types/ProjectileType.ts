35eb0d83c5de321b17b9f4eb4891bb295521db0b
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2021-04-18 16:48:09 -0600

    Remove custom content in prep for merge.

--- src/content/types/ProjectileType.ts
+++
@@ -1,8 +0,0 @@
-import { HealthEntity } from "../../core/components/data/Health";
-
-export class ProjectileType {
-  public static readonly DEFAULT = new ProjectileType();
-  speed = 5; // units per second of projectile
-  damage = 1;
-  splashRadius = 0.01;
-}
